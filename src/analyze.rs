8868038af22b04ca13bb7240bb1f5b6387fd6178
Eliot Courtney <edgeworth.dev@gmail.com>
2022-04-02 08:11:24 +0900

    Refactor

---
+++ src/analyze.rs
@@ -0,0 +1,29 @@
+use std::collections::BTreeMap;
+
+use crate::stats::sample_group::SampleGroup;
+
+// Grapher performs analysis and draws graphs of samples and sample groups.
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct Analyze {
+    groups: BTreeMap<String, SampleGroup>,
+}
+
+impl Analyze {
+    #[must_use]
+    pub fn new() -> Self {
+        Self { groups: BTreeMap::new() }
+    }
+
+    // Adds the sampled value to the Sample with name |id| in group |group|.
+    pub fn add(&mut self, group: &str, id: &str, v: f64) {
+        self.groups.entry(group.to_owned()).or_insert_with(|| SampleGroup::new(group)).add(id, v);
+    }
+
+    pub fn analyse(&self) {
+        for (k, v) in &self.groups {
+            if let Ok(p) = v.analyse() {
+                println!("group {}, p {:.4}:\n{}", k, p, v);
+            }
+        }
+    }
+}
---
+++ src/stats/sample_group.rs
@@ -0,0 +1,42 @@
+use std::collections::BTreeMap;
+
+use eyre::Result;
+
+use crate::stats::sample::Sample;
+
+// Group of samples of the same type to compare together.
+// TODO: Use ANOVA?
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct SampleGroup {
+    name: String,
+    samples: BTreeMap<String, Sample>,
+}
+
+impl std::fmt::Display for SampleGroup {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (k, v) in &self.samples {
+            writeln!(f, "  {}: {}", k, v)?;
+        }
+        Ok(())
+    }
+}
+
+impl SampleGroup {
+    #[must_use]
+    pub fn new(name: &str) -> Self {
+        Self { name: name.to_owned(), samples: BTreeMap::new() }
+    }
+
+    // Adds the sampled value to the Sample with name |id|.cd
+    pub fn add(&mut self, id: &str, v: f64) {
+        self.samples.entry(id.to_owned()).or_insert_with(Sample::new).add(v);
+    }
+
+    pub fn analyse(&self) -> Result<f64> {
+        // TODO: assumes there are two things here
+        let mut iter = self.samples.iter();
+        let a = iter.next().unwrap().1;
+        let b = iter.next().unwrap().1;
+        a.ttest(b)
+    }
+}
